4663c418f9c0a2aebebcfefd6af23c3674695c22
Zakariae Amrani <99502909+Alexo05@users.noreply.github.com>
2025-06-22 14:18:13 +0100

    commit02

--- mes_factures/lib/Screens/Invoices/AddInvoice.dart
+++ mes_factures/lib/Screens/Invoices/AddInvoice.dart
@@ -1,3 +1,4 @@
+import 'package:easy_stepper/easy_stepper.dart';
 import 'package:flutter/material.dart';
 
 class AddInvoice extends StatefulWidget {
@@ -8,6 +9,36 @@ class AddInvoice extends StatefulWidget {
 }
 
 class _AddInvoiceState extends State<AddInvoice> {
+
+  int activeStep = 0;
+  bool isForward = true; 
+
+  void nextStep() {
+      setState(() {
+        if(activeStep == 1 ){
+         
+            activeStep++;
+            isForward = true;
+        }
+        else if (activeStep < 2) {
+          activeStep++;
+          isForward = true;
+        } else {
+         
+        };
+        
+      });
+  }
+
+  void previousStep() {
+    if (activeStep > 0) {
+      setState(() {
+        activeStep--;
+        isForward = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -17,32 +48,146 @@ class _AddInvoiceState extends State<AddInvoice> {
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Row(
-               children: [
-                  GestureDetector(
-                     onTap: () => {
-                       Navigator.pop(context)
-                     },
-                      child: Stack(
-                       alignment: Alignment.center,
-                       children: [
-                         Container(
-                           width: 40,
-                           height: 40,
-                           decoration: BoxDecoration(
-                             borderRadius: BorderRadius.circular(15),
-                             color: Theme.of(context).colorScheme.secondary,
-                           ),
-                           child: Icon(Icons.arrow_back_ios_new, color: Theme.of(context).colorScheme.tertiary, size: 20,),
-                         ),
-                       ],
-                                        ),
+            Column(
+              children: [
+                Row(
+                   children: [
+                      GestureDetector(
+                         onTap: () => {
+                           Navigator.pop(context)
+                         },
+                          child: Stack(
+                           alignment: Alignment.center,
+                           children: [
+                             Container(
+                               width: 40,
+                               height: 40,
+                               decoration: BoxDecoration(
+                                 borderRadius: BorderRadius.circular(15),
+                                 color: Theme.of(context).colorScheme.secondary,
+                               ),
+                               child: Icon(Icons.arrow_back_ios_new, color: Theme.of(context).colorScheme.tertiary, size: 20,),
+                             ),
+                           ],
+                                            ),
+                        ),
+                        SizedBox(width: 10,),
+                         Text("Ajouter une Facture", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Theme.of(context).colorScheme.onSurface),),
+                   ],
+                 ),
+                 SizedBox(height: 10,),
+                 SizedBox(
+                      height: 100,
+                      child: EasyStepper(
+                      activeStep: activeStep,
+                      activeStepTextColor: Colors.black87,
+                      finishedStepTextColor: Colors.black87,
+                      internalPadding: 0,
+                      showLoadingAnimation: false,
+                      stepRadius: 8,
+                      showStepBorder: false,
+                      lineStyle: LineStyle(  
+                        lineType: LineType.normal,
+                        defaultLineColor: Colors.white,
+                        finishedLineColor: Theme.of(context).colorScheme.primary,
+                        lineLength: 130,
+                        lineSpace: 0,
+                        lineThickness: 2,
+                      ),
+                      steps: [
+                        EasyStep(
+                          customStep: CircleAvatar(
+                            radius: 8,
+                            backgroundColor: Colors.white,
+                            child: CircleAvatar(
+                              radius: 7,
+                              backgroundColor:
+                                  activeStep >= 0 ? Theme.of(context).colorScheme.primary: Colors.white,
+                            ),
+                          ),
+                          title: 'Client',
+                        ),
+                        EasyStep(
+                          customStep: CircleAvatar(
+                            radius: 8,
+                            backgroundColor: Colors.white,
+                            child: CircleAvatar(
+                              radius: 7,
+                              backgroundColor:
+                                  activeStep >= 1 ? Theme.of(context).colorScheme.primary: Colors.white,
+                            ),
+                          ),
+                          title: 'Produits',
+                          topTitle: true,
+                        ),
+                        EasyStep(
+                          customStep: CircleAvatar(
+                            radius: 8,
+                            backgroundColor: Colors.white,
+                            child: CircleAvatar(
+                              radius: 7,
+                              backgroundColor:
+                                  activeStep >= 2 ? Theme.of(context).colorScheme.primary: Colors.white,
+                            ),
+                          ),
+                          title: 'Facture',
+                        ),
+                       
+                      ],
+                      onStepReached: (index) =>
+                          setState(() => activeStep = index),
+                                 ),
                     ),
-                    SizedBox(width: 10,),
-                     Text("Ajouter une Facture", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Theme.of(context).colorScheme.onSurface),),
-               ],
-             ),
+                    SizedBox(height: 20,),
+              
+              ],
+            ),
+             Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: previousStep,
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(Colors.white),
+                      shadowColor: WidgetStateProperty.all(Colors.transparent), 
+                    ),
+                    child: Text("Retour" , style: TextStyle(color: Theme.of(context).colorScheme.onSurface),),
+                  ),
+                ),
+                SizedBox(width: 20,),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: nextStep,
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(Colors.transparent),
+                        shadowColor: WidgetStateProperty.all(Colors.transparent), 
+                      ),
+                      child: Text(activeStep < 2 ? "Suivant" : "Enregistrer", 
+                      style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w600,
+                        ),
+                        ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
           ],
         ),
       ),
